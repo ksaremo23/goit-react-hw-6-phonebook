e71bcdfd8cfe5e4ef22a4d67c442017cf3c43663
Leicsha Karen Saremo <120180371+ksaremo23@users.noreply.github.com>
2024-08-10 06:12:43 +0800

    4th

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addContact, deleteContact, setFilter } from '../redux/action';
 import { getContacts, getFilter } from '../redux/selectors';
